a91ec658238a5715657273c926bb0ac116f07592
Barri <info@bdesignstudio.co.uk>
2020-12-17 19:57:02 +0000

    reduce for see if something in balanced

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 ## Test javascript project with jest
+ 
---
+++ reduceUse/index.js
@@ -0,0 +1,12 @@
+function balanceParens(str){
+  return !str.split('').reduce( (prev , char )=>{
+    if(prev <0){ return prev};
+    if(char === "("){ return ++prev};
+    if(char === ")"){ return --prev};
+   return prev;
+    
+  },0);
+}
+
+balanceParens("()")
+balanceParens(")(")
